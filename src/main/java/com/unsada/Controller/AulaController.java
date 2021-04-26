fa6dad90f755c53ba7a6261b86904baa55736c80
Gustavo <gviglesias@alumnos.unsada.edu.ar>
2021-04-26 18:53:51 -0300

    todos

--- src/main/java/com/unsada/Controller/AulaController.java
+++ src/main/java/com/unsada/Controller/AulaController.java
@@ -6,14 +6,21 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import com.unsada.model.Aula;
+import com.unsada.model.Ingresante;
 import com.unsada.service.AulaServiceApi;
 
 @RestController
@@ -70,5 +77,57 @@ public Map<String, Object> dataClase(@PathVariable("id") Integer id) {
 		response.put("success", false);
 		return response;
 	}
+}
+@PostMapping(value = "/create")
+public ResponseEntity<String> create(@RequestBody Aula data) {
+
+	try {
+		aulaServiceApi.save(data);
+		
+		return new ResponseEntity<>("Save successful ", HttpStatus.OK);
+	} catch (Exception e) {
+		
+		return new ResponseEntity<>("" + e, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
+@PutMapping(value = "/update/{id}")
+
+public Map<String, Object> update(@PathVariable("id") Integer id, @RequestBody Aula data) {
+
+	HashMap<String, Object> response = new HashMap<String, Object>();
+
+	try {
+		data.setIdAula(id);
+		aulaServiceApi.save(data);
+		
+		response.put("message", "Successful update");
+		response.put("success", true);
+		return response;
+	} catch (Exception e) {
+		response.put("message", e.getMessage());
+		response.put("success", false);
+		return response;
+	}
+
+}
+@DeleteMapping(value = "/delete/{id}")
+
+public Map<String, Object> update(@PathVariable("id") Integer id) {
+
+	HashMap<String, Object> response = new HashMap<String, Object>();
+
+	try {
+		aulaServiceApi.deleteById(id);
+		;
+		response.put("message", "Successful delete");
+		response.put("success", true);
+		return response;
+	} catch (Exception e) {
+		response.put("message", e.getMessage());
+		response.put("success", false);
+		return response;
+	}
+
 }
 }
--- src/main/java/com/unsada/Controller/SedeController.java
+++ src/main/java/com/unsada/Controller/SedeController.java
@@ -6,13 +6,19 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.unsada.model.Ingresante;
 import com.unsada.model.Sede;
 import com.unsada.service.SedeServiceApi;
 @RestController
@@ -68,5 +74,57 @@ public Map<String, Object> dataClase(@PathVariable("id") Integer id) {
 		response.put("success", false);
 		return response;
 	}
+}
+@PostMapping(value = "/create")
+public ResponseEntity<String> create(@RequestBody Sede data) {
+
+	try {
+		
+		sedeServiceApi.save(data);
+		return new ResponseEntity<>("Save successful ", HttpStatus.OK);
+	} catch (Exception e) {
+		
+		return new ResponseEntity<>("" + e, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
+
+@PutMapping(value = "/update/{id}")
+
+public Map<String, Object> update(@PathVariable("id") Integer id, @RequestBody Sede data) {
+
+	HashMap<String, Object> response = new HashMap<String, Object>();
+
+	try {
+		data.setIdSede(id);
+		sedeServiceApi.save(data);
+		response.put("message", "Successful update");
+		response.put("success", true);
+		return response;
+	} catch (Exception e) {
+		response.put("message", e.getMessage());
+		response.put("success", false);
+		return response;
+	}
+
+}
+@DeleteMapping(value = "/delete/{id}")
+
+public Map<String, Object> update(@PathVariable("id") Integer id) {
+
+	HashMap<String, Object> response = new HashMap<String, Object>();
+
+	try {
+		sedeServiceApi.deleteById(id);
+		;
+		response.put("message", "Successful delete");
+		response.put("success", true);
+		return response;
+	} catch (Exception e) {
+		response.put("message", e.getMessage());
+		response.put("success", false);
+		return response;
+	}
+
 }
 }
