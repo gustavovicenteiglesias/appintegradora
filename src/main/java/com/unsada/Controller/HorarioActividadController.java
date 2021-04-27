79b2e78d955519641f4719423863973e2132e3a3
Thomas Diaz <thomydiaz00@gmail.com>
2021-04-27 14:11:55 -0300

    Put & Delete para horarios

--- src/main/java/com/unsada/Controller/HorarioActividadController.java
+++ src/main/java/com/unsada/Controller/HorarioActividadController.java
@@ -11,9 +11,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +58,41 @@ public class HorarioActividadController{
             return new ResponseEntity<>("" + e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+    }
+    @PutMapping(value = "/update/{id}")
+    public Map<String, Object> update(@PathVariable("id") Integer id, @RequestBody Horariosactividad data) {
+
+        HashMap<String, Object> response = new HashMap<String, Object>();
+
+        try {
+            data.setIdHorarioActividad(id);;
+            horariosServiceApi.save(data);
+            response.put("message", "Successful update");
+            response.put("success", true);
+            return response;
+        } catch (Exception e) {
+            response.put("message", e.getMessage());
+            response.put("success", false);
+            return response;
+        }
+
+    }
+    @DeleteMapping(value = "/delete/{id}")
+    public Map<String, Object> delete(@PathVariable("id") Integer id) {
+
+        HashMap<String, Object> response = new HashMap<String, Object>();
+    
+        try {
+            horariosServiceApi.deleteById(id);
+            response.put("message", "Successful delete");
+            response.put("success", true);
+            return response;
+        } catch (Exception e) {
+            response.put("message", e.getMessage());
+            response.put("success", false);
+            return response;
+        }
+    
     }
     @GetMapping(value = "/all")
     public Map<String, Object> listhorarios() {
